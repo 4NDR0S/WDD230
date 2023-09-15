5cb345c99cba3b6ecc316652e13cc1c77d357b06
4NDR0S <andross.vela@gmail.com>
2023-09-14 20:53:53 -0500

    WDD 230

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// Obtner el elemento del año actual y establece su valor
+var year = document.getElementById("year");
+year.textContent = new Date().getFullYear();
+
+// Obtener el elemento de última modificación y establece su valor
+var lastModified = document.getElementById("lastModified");
+lastModified.textContent = document.lastModified;
