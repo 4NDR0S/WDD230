89027c9aca19e38cdd531d68e5160ed130a76ed3
4NDR0S <andross.vela@gmail.com>
2023-10-18 18:10:37 -0500

    windchill

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,6 +1,6 @@
 var temp = 73;
-var wSpeed = 10;
-var windchill = (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(wSpeed, 0.16)) + (0.4275 * temp * Math.pow(wSpeed, 0.16));
+var windS = 10;
+var windchill = (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(windS, 0.16)) + (0.4275 * temp * Math.pow(windS, 0.16));
 
 var windchill = Math.round(windchill);
 
@@ -8,4 +8,4 @@ document.getElementById("windchill").innerHTML = windchill;
 
 // temp and windspeed values in html
 document.getElementById("temp").innerHTML = temp;
-document.getElementById("windSpeed").innerHTML = wSpeed;
+document.getElementById("windSpeed").innerHTML = windS;
