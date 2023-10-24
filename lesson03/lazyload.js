6f34deeffc2c4f3a92b652b1f904962c7d320a78
4NDR0S <andross.vela@gmail.com>
2023-10-23 19:14:26 -0500

    Lazy Loading

--- lesson03/lazyload.js
+++ lesson03/lazyload.js
@@ -1,7 +1,3 @@
-
-
-/* <for the footer> */
-
 // Obtner el elemento del año actual y establece su valor
 var year = document.getElementById("year");
 year.textContent = new Date().getFullYear();
@@ -9,3 +5,4 @@ year.textContent = new Date().getFullYear();
 // Obtener el elemento de última modificación y establece su valor
 var lastModified = document.getElementById("lastModified");
 lastModified.textContent = document.lastModified;
+
