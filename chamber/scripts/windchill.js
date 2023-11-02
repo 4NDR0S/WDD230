c290265ed417ac640e204f33afd11347f8139635
4NDR0S <andross.vela@gmail.com>
2023-11-01 23:00:44 -0500

    fixing w6 hero and windchill

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,8 +1,18 @@
-var temp = 73;
+var temp = 70;
 var windS = 10;
-var windchill = (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(windS, 0.16)) + (0.4275 * temp * Math.pow(windS, 0.16));
 
-var windchill = Math.round(windchill);
+// var windchill = (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(windS, 0.16)) + (0.4275 * temp * Math.pow(windS, 0.16));
+
+// var windchill = Math.round(windchill);
+var windchill = 0
+if (temp > 50 || windS < 3) {
+    // document.getElementById("windchill").innerHTML = "N/A";
+    windchill = "N/A"
+} else {
+    windchill = (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(windS, 0.16)) + (0.4275 * temp * Math.pow(windS, 0.16));
+
+    windchill = Math.round(windchill);
+}
 
 document.getElementById("windchill").innerHTML = windchill;
 
